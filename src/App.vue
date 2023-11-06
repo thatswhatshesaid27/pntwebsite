cb9cffb62151c8c32e38b488e6d5536dabd2a867
Yash <yashdubale2002@gmail.com>
2023-11-06 12:17:37 +0530

    COMMITING

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Home/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
@@ -19,8 +20,15 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* text-align: center; */
+  color: white;
+  /* margin-top: 60px; */
+  padding: 0;
+  margin: 0;
+}
+body {
+  background: #080808;
+  padding: 0;
+  margin: 0;
 }
 </style>
